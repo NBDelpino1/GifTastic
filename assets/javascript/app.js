506c120b8d035ed79eb737a220857fb571bc114b
Nicholas Delpino <Nicky@Nicholas-MacBook-Pro.local>
2016-08-09 23:57:04 -0400

    cleaned up jQuery, minor cosmetic touches

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,64 +1,42 @@
 var topics = ['Michael Jordan', 'Austin Powers', 'Optimus Prime', 'Family Guy'];
 renderButtons();
-//Ajax call
-$('.giphy').on('click', function() {
-    $('#results').empty();
-        $.ajax({
-            url: "http://api.giphy.com/v1/gifs/search?api_key=dc6zaTOxFJmzC",
-            data: {
-                limit: 5,
-                api_key: "dc6zaTOxFJmzC",
-                q: $(this).attr("data-name")
-            },
-            method: 'GET'
-        }).done(function(response) {
-            console.log(response);
-            $.each(response.data, function(key, gif) {
-                $("#results").append('<iframe src="' + gif.embed_url + ' "/>');
-            })
-        });
-    })
-    ///function generates new giphys for button clicked  
+//listen for anything with class of giphy button div
+$('#gifbutton').on('click', '.giphy', function() {
+    $('#results').empty();//empty results div
+    $.ajax({//run query
+        url: "http://api.giphy.com/v1/gifs/search?api_key=dc6zaTOxFJmzC",
+        data: {
+            limit: 5,
+            api_key: "dc6zaTOxFJmzC",
+            q: $(this).attr("data-name")//refers to data-name attr of button clicked
+        },
+        method: 'GET'
+    }).done(function(response) {//when query done returns response
+        console.log(response);
+        $.each(response.data, function(key, gif) {
+            $("#results").append('<iframe src="' + gif.embed_url + ' "/>');//display image in results div
+        })
+    });
+    });
+//grab entry from text box and create button in memory 
 $('#addbutton').on('click', function() {
-        //get input from textbox
-        var newGif = $('#textBox').val().trim();
-        //call addNewButton function and pass in what was entered in the textbox
-        addNewButton(newGif);
-        // We have this line so that users can hit "enter" instead of clicking on ht button and it won't move to the next page
-        return false;
+    var newGif = $('#textBox').val().trim();//get input from textbox
+    addNewButton(newGif);//pass new entry into addNewButton function so new button can be generated
+    // We have this line so that users can hit "enter" instead of clicking on ht button and it won't move to the next page
+    return false;
     })
     //function adds new buttons
-
-function addNewButton(name) {
-        //create actual button
-        var newButton = $('<button>')
-            //give button a class         
-        newButton.addClass('giphy');
-        //give button an attribute
-        newButton.attr('data-name', name);
-        //when button clicked run ajax call
-        $(newButton).click(function() {
-            $('#results').empty();
-            $.ajax({
-                url: "http://api.giphy.com/v1/gifs/search?api_key=dc6zaTOxFJmzC",
-                data: {
-                    limit: 5,
-                    api_key: "dc6zaTOxFJmzC",
-                    q: name
-                },
-                method: 'GET'
-            }).done(function(response) {
-                console.log(response);
-                $.each(response.data, function(key, gif) {
-                    $("#results").append('<iframe src="' + gif.embed_url + ' "/>');
-                })
-            });
-        })
-        newButton.text(name);
-        $('#gifbutton').append(newButton);
+    function addNewButton(name) {
+    //create actual button
+    var newButton = $('<button>')
+        //give button a class         
+    newButton.addClass('giphy');
+    //give button an attribute
+    newButton.attr('data-name', name);
+    newButton.text(name);
+    $('#gifbutton').append(newButton);
     }
-    //function displays new button
-
+//display new button
 function renderButtons() {
     // Deletes the Giphy prior to adding new Giphys (this is necessary otherwise you will have repeat buttons)
     $('#results').empty();
@@ -73,4 +51,4 @@ function renderButtons() {
         //display new button        
         $('#gifbutton').append(newButton);
     }
-}
+    }
