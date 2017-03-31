874cba274e71867cb30ab3f2b556c501b561776b
Nicholas Delpino <NBDelpino4@gmail.com>
2017-03-30 23:09:49 -0400

    figured out how to make the iframes popluate in a uniformed way better

--- assets/javascript/materialize.js
+++ assets/javascript/materialize.js
@@ -11,9 +11,9 @@ $(function(){
 
 
 
-$('#submitbutton').on('click', function () {
+$('#submitButton').on('click', function () {
 
-    // 1. Empty the results div so that only 15 gifs appear at any given time
+    // 1. Empty the results div so that only X gifhs appear at any given time
     $('#results').empty();
 
     // 2. Get the user's query from the form and store it into a variable called userQuery
@@ -27,17 +27,39 @@ $('#submitbutton').on('click', function () {
 
     // 3. Now that we have the user's query we are going to run the AJAX call and pass it in
 
+    // $.ajax({
+    //
+    //     url : "http://api.giphy.com/v1/gifs/search?api_key=dc6zaTOxFJmzC",
+    //     data : {
+    //
+    //         // Set the limit of how many gifs the user sees at a time
+    //
+    //         limit : 10,
+    //         api_key : "dc6zaTOxFJmzC",
+    //
+    //         // Pass in the user's query (variable userQuery)
+    //         q : userQuery
+    //     },
+    //     method : 'GET'
+    //
+    //     // 4. After the AJAX call is done we want to pass the response (res) into a function so we can do
+    //     // something with it
+    //
+    // })
+    //
+    //
+    //
+    //
+    //
+    //
     $.ajax({
 
         url : "http://api.giphy.com/v1/gifs/search?api_key=dc6zaTOxFJmzC",
         data : {
 
-            // Set the limit of how many gifs the user sees at a time
-
-            limit : 84,
+            limit : 20,
             api_key : "dc6zaTOxFJmzC",
 
-            // Pass in the user's query (variable userQuery)
             q : userQuery
         },
         method : 'GET'
@@ -46,6 +68,8 @@ $('#submitbutton').on('click', function () {
         // something with it
 
     }).done(function (res) {
+
+        console.log(res.data);
 
         console.log("Okay cool I'm back!");
 
@@ -60,7 +84,13 @@ $('#submitbutton').on('click', function () {
 
             console.log("Time to show you the goods, go check out the browser!");
 
-            $("#results").append('<iframe height="170px" width="248px" src = " ' +  gif.embed_url +' " />');
+
+            // Grab the ratings from the data returned and denerate some html to display to user
+            $("#imagesContainer").append(' <div class="col s4 m3"><div class="video-container"><iframe  src=" ' + gif.embed_url + ' " frameborder="0"></iframe></div></div>');
+
+            // TODO - need to append this to its respective iframe
+            $("#ratingsContainer").append('<div class="col s3"><p>Rated: ' + gif.rating + '</p></div>');
+
             // $("#results").append('<iframe height="300px" width="300px" src = " ' +  gif.embed_url +' " />');
 
             // Check to see what the rating of each response is (may do something with this later)
